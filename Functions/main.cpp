d67bc33da9d79c6e9b89a3dfb64ff252f16cac29
surov <surov@LAPTOP-LMI8CJ6S>
2022-10-19 16:57:34 +0200

    Add project files.

---
+++ Functions/main.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+int Add(int a, int b)
+{
+	int sum = a + b;
+	return sum;
+}
+int Sub(int a, int b)
+{
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	return a * b;
+}
+double Div(int a, int b)
+{
+	return (double)a / b;
+}
+
+
+void main()
+{
+	int a, b;
+	cout << sizeof(int) << endl; // оператор sizeof() возвращает размер типа или значения в Bytes. 
+	cout << "Type two numbers: "; cin >> a >> b;
+	int c = Add(a, b);
+	cout << a << "+" << b << "=" << c << endl;
+	cout << a << "-" << b << "=" << Sub(a,b) << endl;
+	cout << a << "*" << b << "=" << Mul(a,b) << endl;
+	cout << a << "/" << b << "=" << Div(a,b) << endl;
+
+}
